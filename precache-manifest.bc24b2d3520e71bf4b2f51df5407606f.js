cb9e0897ca7d9ae831a561333c042b68e89aa8a8
Lean <leanjunio@live.com>
2020-07-06 15:41:05 -0400

    Updates

---
+++ precache-manifest.bc24b2d3520e71bf4b2f51df5407606f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cda54e3541e4309a5bdd759cc7ead910",
+    "url": "/leanjunio/photos/index.html"
+  },
+  {
+    "revision": "e40968940bc04d21ff9a",
+    "url": "/leanjunio/photos/static/css/2.9ff99f11.chunk.css"
+  },
+  {
+    "revision": "95757b54e199e7170668",
+    "url": "/leanjunio/photos/static/css/main.75df9f48.chunk.css"
+  },
+  {
+    "revision": "e40968940bc04d21ff9a",
+    "url": "/leanjunio/photos/static/js/2.586f34e1.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/leanjunio/photos/static/js/2.586f34e1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "95757b54e199e7170668",
+    "url": "/leanjunio/photos/static/js/main.489ae270.chunk.js"
+  },
+  {
+    "revision": "b7916bf0cac9e634a6f7",
+    "url": "/leanjunio/photos/static/js/runtime-main.335b4f08.js"
+  }
+]);
